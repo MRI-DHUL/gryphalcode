baa8e7c1b519e81de48cb461151385c47f6aea1b
Mridhul M <mridhulvinu123@gmail.com>
2026-09-07 15:58:00 +0530

    Separate browser router from renderable route tree

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Footer from './components/Footer'
-import Home from './pages/Home'
-import About from './pages/About'
-import Services from './pages/Services'
-import ServiceDetail from './pages/ServiceDetail'
-import CaseStudies from './pages/CaseStudies'
-import Blog from './pages/Blog'
-import Events from './pages/Events'
-import Contact from './pages/Contact'
-import NotFound from './pages/NotFound'
+import { BrowserRouter } from 'react-router-dom'
+import AppRoutes from './AppRoutes'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <div className="site-shell">
-        <Navbar />
-        <main>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/services" element={<Services />} />
-            <Route path="/services/:slug" element={<ServiceDetail />} />
-            <Route path="/case-studies" element={<CaseStudies />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/events" element={<Events />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <AppRoutes />
     </BrowserRouter>
   )
 }
